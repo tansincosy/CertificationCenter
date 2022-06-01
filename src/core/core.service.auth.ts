7ece08a101a3a1bc433e53c9a9ff2645bef371b6
yy921010 <charlesmaxwellyoung@gmail.com>
2022-06-01 21:43:53 +0800

    ✨ feat: 初步构建oauth2 校验流程

--- src/core/core.service.auth.ts
+++ src/core/core.service.auth.ts
@@ -1,11 +1,227 @@
-import OAuth2Server, {
+import { Logger, LoggerService } from '@/common/log4j/log4j.service';
+import {
+  AuthorizationCode,
+  AuthorizationCodeModel,
   Client,
   Falsey,
-  InvalidTokenError,
   PasswordModel,
   RefreshToken,
   RefreshTokenModel,
   Token,
   User,
 } from 'oauth2-server';
-export class CoreModelService implements PasswordModel, RefreshTokenModel {}
+import { PrismaService } from '@/common/database/prisma.service';
+import { md5, secretMask, toJSON, toObject } from '@/util/help.util';
+export class CoreModelService
+  implements PasswordModel, RefreshTokenModel, AuthorizationCodeModel
+{
+  private LOG: Logger;
+  constructor(
+    private readonly logService: LoggerService,
+    private readonly prismaService: PrismaService,
+  ) {
+    this.LOG = this.logService.getLogger(CoreModelService.name);
+  }
+
+  async getAuthorizationCode(
+    authorizationCode: string,
+  ): Promise<Falsey | AuthorizationCode> {
+    this.LOG.debug(
+      '[getAuthorizationCode] authorizationCode = %s',
+      authorizationCode,
+    );
+    const authorizationObj = await this.prismaService.oAuthCode.findFirst({
+      where: {
+        code: authorizationCode,
+      },
+    });
+    if (!authorizationObj || !authorizationCode) {
+      return false;
+    }
+
+    return null;
+  }
+
+  async saveAuthorizationCode(
+    code: Pick<
+      AuthorizationCode,
+      'authorizationCode' | 'expiresAt' | 'redirectUri' | 'scope'
+    >,
+    client: Client,
+    user: User,
+  ): Promise<Falsey | AuthorizationCode> {
+    return false;
+  }
+
+  async revokeAuthorizationCode(code: AuthorizationCode): Promise<boolean> {
+    await this.prismaService.oAuthCode.delete({
+      where: {
+        code: code.authorizationCode,
+      },
+    });
+    this.LOG.debug('[revokeAuthorizationCode] delete code success');
+
+    return true;
+  }
+
+  async revokeToken(token: Token | RefreshToken): Promise<boolean> {
+    if (!token) {
+      this.LOG.warn('[revokeToken] token is null');
+      return false;
+    }
+
+    const delAccessToken = this.prismaService.oAuthAccessToken.delete({
+      where: {
+        tokenId: token.accessToken,
+      },
+    });
+
+    const delRefreshToken = this.prismaService.oAuthRefreshToken.delete({
+      where: {
+        tokenId: token.refreshToken,
+      },
+    });
+
+    await this.prismaService.$transaction([delAccessToken, delRefreshToken]);
+    this.LOG.debug('[revokeToken] process end');
+    return true;
+  }
+
+  async getUser(username: string, password: string): Promise<User | Falsey> {
+    this.LOG.debug(
+      '[getUser] username = %s, password = %s',
+      username,
+      secretMask(password),
+    );
+
+    return {
+      id: '',
+      username: '',
+    };
+  }
+
+  /**
+   * 验证设备终端
+   * @param clientId 设备id
+   * @param clientSecret 设备密钥
+   * @returns
+   */
+
+  async getClient(
+    clientId: string,
+    clientSecret: string,
+  ): Promise<Client | Falsey> {
+    this.LOG.debug(
+      '[getClient] clientId = %s , clientSecret = %s',
+      secretMask(clientId),
+      secretMask(clientSecret),
+    );
+    const client = await this.prismaService.oAuthClientDetails.findFirst({
+      where: {
+        id: clientId,
+      },
+    });
+
+    if (!client || client.clientSecret !== clientSecret) {
+      this.LOG.warn('client is not found! or client secret is valid');
+      return false;
+    }
+
+    return {
+      id: client.id,
+      clientSecret: client.clientSecret,
+      grants: client.authorizedGrantTypes?.split(',') || '',
+      redirectUris: client.webServerRedirectUri || '',
+      scope: client.scope?.split(',') || '',
+      accessTokenLifetime: client.accessTokenValidity || 0,
+      refreshTokenLifetime: client.refreshTokenValidity || 0,
+    };
+  }
+  async saveToken(
+    token: Token,
+    client: Client,
+    user: User,
+  ): Promise<Falsey | Token> {
+    this.LOG.debug('[saveToken] process begin');
+    const { id } = await this.prismaService.oAuthAccessToken.create({
+      data: {
+        username: user.username,
+        clientId: client.id,
+        tokenId: token.accessToken,
+        refreshToken: token.refreshToken,
+        token: toJSON(token),
+        //根据当前的username、client_id与scope通过MD5加密生成该字段的值
+        authenticationId: md5(
+          user.username + client.id + token.scope,
+          user.username,
+        ),
+      },
+      select: {
+        id: true,
+      },
+    });
+    this.LOG.debug('[saveToken] process save refresh token');
+    await this.prismaService.oAuthRefreshToken.create({
+      data: {
+        tokenId: token.refreshToken,
+        token: toJSON(token),
+      },
+    });
+
+    this.LOG.debug('[saveToken] process end');
+    if (!id) {
+      this.LOG.warn('[saveToken] save token failed!');
+      return false;
+    }
+    return {
+      ...token,
+      user,
+      client,
+    };
+  }
+
+  async getRefreshToken(refreshToken: string): Promise<Falsey | RefreshToken> {
+    this.LOG.debug('[getRefreshToken] process begin');
+    const refreshTokenObj =
+      await this.prismaService.oAuthRefreshToken.findFirst({
+        where: {
+          tokenId: refreshToken,
+        },
+      });
+
+    const tokenObj = toObject<RefreshToken>(refreshTokenObj.token);
+    return {
+      ...tokenObj,
+      refreshTokenExpiresAt: new Date(tokenObj.refreshTokenExpiresAt),
+    };
+  }
+
+  async getAccessToken(accessToken: string): Promise<Falsey | Token> {
+    this.LOG.debug(
+      '[getAccessToken] accessToken = %s',
+      secretMask(accessToken),
+    );
+    const token = await this.prismaService.oAuthAccessToken.findFirst({
+      where: {
+        tokenId: accessToken,
+      },
+    });
+
+    if (!token) {
+      this.LOG.warn('[getAccessToken] token is not found!');
+      return false;
+    }
+
+    const tokenObj = toObject<Token>(token.token);
+
+    return {
+      ...tokenObj,
+      accessToken,
+      accessTokenExpiresAt: new Date(tokenObj.accessTokenExpiresAt),
+      scope: tokenObj.scope,
+    };
+  }
+  async verifyScope(token: Token, scope: string | string[]): Promise<boolean> {
+    return true;
+  }
+}
---
+++ src/util/help.util.ts
@@ -0,0 +1,131 @@
+import {
+  createCipheriv,
+  createDecipheriv,
+  createHash,
+  pbkdf2,
+  randomBytes,
+  randomUUID,
+} from 'crypto';
+import { isEmpty } from 'lodash';
+const algorithm = 'aes-256-cbc';
+const iv = randomBytes(16);
+
+export function secretMask(cc: string, num = 4, len = 32, mask = '*'): string {
+  return cc.slice(-num).padStart(len, mask);
+}
+
+export function JSON2Object<T>(jsonStringy: string, defaultValue = {}): T {
+  try {
+    defaultValue = JSON.parse(jsonStringy);
+  } catch (error) {}
+
+  return defaultValue as T;
+}
+
+export function randomTimeStampSeconds(min: number, max: number) {
+  return parseInt(Math.random() * (max - min) + min + '');
+}
+
+export function encrypt(key, text) {
+  // Creating Cipheriv with its parameter
+  const cipher = createCipheriv(algorithm, Buffer.from(key), iv);
+
+  // Updating text
+  let encrypted = cipher.update(text);
+
+  // Using concatenation
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  // Returning iv and encrypted data
+  const ivStr: string = iv.toString('hex');
+  const encryptedData: string = encrypted.toString('hex');
+  return `${encryptedData}:${ivStr}`;
+}
+
+export function decrypt(key, encryptedDataParam) {
+  if (!encryptedDataParam) {
+    return '';
+  }
+  const [encryptedData, textIv] = encryptedDataParam.split(':');
+  const bufferIV = Buffer.from(textIv, 'hex');
+  const encryptedText = Buffer.from(encryptedData, 'hex');
+  // Creating Decipher
+  const decipher = createDecipheriv(algorithm, Buffer.from(key), bufferIV);
+
+  // Updating encrypted text
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
+
+export function toJSON(params: any): string {
+  if (isEmpty(params)) {
+    return '';
+  }
+  return JSON.stringify(params);
+}
+
+export function toObject<T>(params: string): T {
+  if (isEmpty(params)) {
+    return {} as T;
+  }
+  try {
+    return JSON.parse(params) as T;
+  } catch (error) {
+    return {} as T;
+  }
+}
+
+export function btoa(botaStr): string {
+  return Buffer.from(botaStr).toString('base64');
+}
+
+export function atob(b64Encoded): string {
+  return Buffer.from(b64Encoded, 'base64').toString();
+}
+
+export function joinKey(...keys: string[]) {
+  return keys.reduce((total, cur) => {
+    return total + ':' + cur;
+  });
+}
+
+/**
+ *
+ * @param defaultStr
+ * @param salt
+ * @returns
+ */
+export function md5(defaultStr = '', salt = ''): string {
+  const saltStr = `${defaultStr}:${salt}`;
+  const md5 = createHash('md5');
+  return md5.update(saltStr).digest('hex');
+}
+/**
+ * pbkdf2 加密
+ * @param userPassword
+ * @returns
+ */
+export function encryptedWithPbkdf2(userPassword: string): Promise<string> {
+  //盐值随机
+  const salt = randomUUID();
+  let primaryDriverKey = '';
+  return new Promise((resolve, reject) => {
+    pbkdf2(userPassword, salt, 1000, 64, 'sha512', (err, derivedKey) => {
+      if (err) {
+        primaryDriverKey = '';
+        reject(primaryDriverKey);
+      } else {
+        primaryDriverKey = derivedKey.toString('hex');
+        resolve(primaryDriverKey);
+      }
+    });
+  });
+}
+
+export function moreThOne(param: any): boolean {
+  if (Array.isArray(param)) {
+    return param.length > 0;
+  }
+  return false;
+}
